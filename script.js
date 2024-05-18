aad48502574d232c3b11466738703658a5e28999
Mithramadhu66 <mitha0179@gmail.com>
2024-05-18 11:52:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: "smooth"
+                });
+            } else {
+                window.location.href = this.getAttribute("href");
+            }
+        });
+    });
+});
